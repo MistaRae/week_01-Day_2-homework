1387a2ec63a80c9844bc9ff02eacd95322fff302
MistaRae <itsamistarae@gmail.com>
2021-03-31 10:33:40 +0100

    tidied up a bit

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -1,57 +1,57 @@
 users = {
-  "Jonathan": {
-    "twitter": "jonnyt",
-    "lottery_numbers": [6, 12, 49, 33, 45, 20],
-    "home_town": "Stirling",
-    "pets": [
-    {
-      "name": "fluffy",
-      "species": "cat"
+    "Jonathan": {
+        "twitter": "jonnyt",
+        "lottery_numbers": [6, 12, 49, 33, 45, 20],
+        "home_town": "Stirling",
+        "pets": [
+            {
+                "name": "fluffy",
+                "species": "cat"
+            },
+            {
+                "name": "fido",
+                "species": "dog"
+            },
+            {
+                "name": "spike",
+                "species": "dog"
+            }
+        ]
     },
-    {
-      "name": "fido",
-      "species": "dog"
+    "Erik": {
+        "twitter": "eriksf",
+        "lottery_numbers": [18, 34, 8, 11, 24],
+        "home_town": "Linlithgow",
+        "pets": [
+            {
+                "name": "nemo",
+                "species": "fish"
+            },
+            {
+                "name": "kevin",
+                "species": "fish"
+            },
+            {
+                "name": "spike",
+                "species": "dog"
+            },
+            {
+                "name": "rupert",
+                "species": "parrot"
+            }
+        ]
     },
-    {
-      "name": "spike",
-      "species": "dog"
+    "Avril": {
+        "twitter": "bridgpally",
+        "lottery_numbers": [12, 14, 33, 38, 9, 25],
+        "home_town": "Dunbar",
+        "pets": [
+            {
+                "name": "monty",
+                "species": "snake"
+            }
+        ]
     }
-  ]
-  },
-  "Erik": {
-    "twitter": "eriksf",
-    "lottery_numbers": [18, 34, 8, 11, 24],
-    "home_town": "Linlithgow",
-    "pets": [
-    {
-      "name": "nemo",
-      "species": "fish"
-    },
-    {
-      "name": "kevin",
-      "species": "fish"
-    },
-    {
-      "name": "spike",
-      "species": "dog"
-    },
-    {
-      "name": "rupert",
-      "species": "parrot"
-    }
-  ]
-  },
-  "Avril": {
-    "twitter": "bridgpally",
-    "lottery_numbers": [12, 14, 33, 38, 9, 25],
-    "home_town": "Dunbar",
-    "pets": [
-      {
-        "name": "monty",
-        "species": "snake"
-      }
-    ]
-  }
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
@@ -79,22 +79,22 @@ users["Erik"]["lottery_numbers"].sort()
 print(users["Erik"]["lottery_numbers"][0])
 avrils_even_numbers = []
 for number in users["Avril"]["lottery_numbers"]:
-  if number % 2 == 0:
-    avrils_even_numbers.append(number)
+    if number % 2 == 0:
+        avrils_even_numbers.append(number)
 print(avrils_even_numbers)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 users["Erik"]["lottery_numbers"].append(7)
-      #re-sorting the numbers, just because.
+# re-sorting the numbers, just because.
 users["Erik"]["lottery_numbers"].sort()
 print(users["Erik"]["lottery_numbers"])
 # 8. Change Erik's hometown to Edinburgh
-users["Erik"]["home_town"] = "Edinburgh" 
+users["Erik"]["home_town"] = "Edinburgh"
 print(users["Erik"]["home_town"])
 # 9. Add a pet dog to Erik called "Fluffy"
 users["Erik"]["pets"].append({
-    "name" : "Fluffy",
-    "species" : "dog"
-  }
+    "name": "Fluffy",
+    "species": "dog"
+}
 )
 print(users["Erik"]["pets"][4])
 # 10. Add another person to the users dictionary
@@ -103,10 +103,9 @@ users["Mark"] = {
     "lottery_numbers": [12, 14, 33, 38, 9, 25],
     "home_town": "Aberdeen",
     "pets": [
-      {
-        "name": "Juno",
-        "species": "dog"
-      }
+        {
+            "name": "Juno",
+            "species": "dog"
+        }
     ]
 }
-
